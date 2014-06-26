d19753aa3e2432245e649be03f0d5192cde8c61f
qinshiwei <qinshiwei@dianxinos.com>
2014-06-26 16:04:06 +0800

    Add position

--- global_function.py
+++ global_function.py
@@ -2,11 +2,16 @@ import ConfigParser
 
 
 def get_config(section, conf_name):
+    global config 
     config = ConfigParser.ConfigParser()
     config.read("conf/config.ini")
     conf = config.get(section, conf_name)
     return conf
 
+def set_config(section, key, value):
+	config.set(section, key, value)
+	config.write(open("conf/config.ini", "w"))
+
 TAOBAO_API = get_config("taobaoapi", "taobao_api")
-IP_START = get_config("ip_range", "ip_start")
+IP_START = get_config("position", "ip")
 IP_END = get_config("ip_range", "ip_end")
--- ip_range.py
+++ ip_range.py
@@ -3,6 +3,7 @@ import socket, struct
 import global_function as func
 import urllib2
 import json
+import time
 
 FILE = open("data/ip_lib.txt", "w")
 
@@ -14,11 +15,11 @@ def ip_to_long(ip):
     return struct.unpack("!L", packedIP)[0]
 
 
-def long_to_ip(long):
+def long_to_ip(long_ip):
 	"""
     	Convert an Long to ip
     	"""
-	return socket.inet_ntoa(struct.pack('!L', long))
+	return socket.inet_ntoa(struct.pack('!L', long_ip))
 
 def get_location(api, long_ip):
 	string_ip = long_to_ip(long_ip)
@@ -38,7 +39,7 @@ def parse_json(data):
     	location_info = ""
     	for subkey in subvalue:
 		 location_info = "%s%s:%s|" % (location_info,subkey, subvalue[subkey])
-	write_to_file(location_info)
+	write_to_file(location_info + "\n")
 	print location_info
 
 def write_to_file(location_info):
@@ -49,6 +50,9 @@ def main():
     api = func.TAOBAO_API
     ip_range = [ip_to_long(func.IP_START), ip_to_long(func.IP_END)]
     for long_ip in xrange(ip_range[0], ip_range[1]):
+    	position_ip = long_to_ip(long_ip)
+    	func.set_config("position", "ip", position_ip)
+    	time.sleep(0.1)
     	parse_json(request_url(get_location(api, long_ip)))
 
 def end():
